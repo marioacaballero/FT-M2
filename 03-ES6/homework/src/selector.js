09026612f36c761d9f89fb183970d38cfd8d757a
Mario <mario.caballero.948@gmail.com>
2022-04-27 20:31:19 -0300

    Homework 03 done

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,7 +9,17 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+  if(matchFunc(startEl)) resultSet.push(startEl);
+
+  for(let i=0; i<startEl.children.length; i++){
+    let child = startEl.children[i];
+    let found = traverseDomAndCollectElements(matchFunc, child)
+    resultSet = [...resultSet, ...found];
+  }
+
+  //con el for genero el caso de corte de la recursión, puede ser que no entra porque no tiene elementos hijos, o porque se termina de recorrer todos los hijos. Cualquiera de las 2 opciones corta.
+
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -24,7 +34,7 @@ var selectorTypeMatcher = function(selector) {
   } else if(selector[0] === '.'){
       return 'class';
     } else {
-      for(let i=0; i<selector.length; i++){
+      for(let i=1; i<selector.length; i++){
         if(selector[i] === '.'){
           return 'tag.class';
         }
@@ -45,13 +55,24 @@ var matchFunctionMaker = function(selector) {
   var matchFunction;
 
   if (selectorType === "id") { 
-    
+    matchFunction = (element) => '#' + element.id === selector;
   } else if (selectorType === "class") {
-    
+    matchFunction = (element) => {
+      let classes = element.classList;
+      for(let i=0; i<classes.length; i++){
+        if("." + classes[i] === selector){
+          return true;
+        }
+      }
+      return false;
+    }
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = (element) => {
+      let [t, c] = selector.split('.');
+      return matchFunctionMaker(t)(element) && matchFunctionMaker('.' + c)(element); //porque la matchFunctionMaker(t) me devuelve una función, entonces puedo invocarla con (element). Para el caso de la clase debo agregarle el punto ya que con el split se lo saqué.
+    }
   } else if (selectorType === "tag") {
-    
+    matchFunction = (element) => element.tagName.toLowerCase() === selector; //si uso localName directamente lo da en minuscula y no necesito el toLowerCase
   }
   return matchFunction;
 };
