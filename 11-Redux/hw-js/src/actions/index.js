a40e7209ac8f4d3bb7b22c2b5dd43ea67581562f
Mario <mario.caballero.948@gmail.com>
2022-05-18 21:58:11 -0300

    Homework 11 done

--- 11-Redux/hw-js/src/actions/index.js
+++ 11-Redux/hw-js/src/actions/index.js
@@ -1,15 +1,29 @@
-const { INCREMENTO, DECREMENTO } = require('../action-types');
+const { INCREMENTO, DECREMENTO, IMPAR, ASYNC } = require("../action-types");
 
-// Nuestras actions (action creators) devolverán un paquete de actions que nuestro reducer recibirá. 
-// ¿Cómo es el paquete de acción? Tengan en cuenta que el creador de la acción no es en absoluto responsable 
+// Nuestras actions (action creators) devolverán un paquete de actions que nuestro reducer recibirá.
+// ¿Cómo es el paquete de acción? Tengan en cuenta que el creador de la acción no es en absoluto responsable
 // de manejar ninguna de las lógicas actuales de actualización del store central de Redux.
 // Eso se lo deja al reducer(s).
 
-const incremento = null;
+const incremento = function () {
+  return { type: INCREMENTO };
+};
 
-const decremento = null;
+const decremento = function () {
+  return { type: DECREMENTO };
+};
+
+const impar = function () {
+  return { type: IMPAR };
+};
+
+const async = function () {
+  return { type: ASYNC };
+};
 
 module.exports = {
   incremento,
-  decremento
-}
+  decremento,
+  impar,
+  async,
+};
--- 11-Redux/hw-js/src/reducer/index.js
+++ 11-Redux/hw-js/src/reducer/index.js
@@ -1,15 +1,44 @@
-const { INCREMENTO, DECREMENTO } = require('../action-types');
+const { INCREMENTO, DECREMENTO, IMPAR, ASYNC } = require("../action-types");
 
 const initialState = {
-  contador: 0
-}
+  contador: 0,
+};
 
 // Nuestro reducer que maneja nuestros dos casos de acción incremento y decremento.
-// Recibe el estado de nuestro store, junto con una action creada por nuestro action creator. 
+// Recibe el estado de nuestro store, junto con una action creada por nuestro action creator.
 // ¿Qué tiene que hacer el reducer con el contador de cada caso?
 
 function contador(state = initialState, action) {
-  
+  if (action.type === INCREMENTO) {
+    return {
+      ...state,
+      contador: state.contador + 1,
+    };
+  } else if (action.type === DECREMENTO) {
+    return {
+      ...state,
+      contador: state.contador - 1,
+    };
+  } else if (action.type === IMPAR) {
+    if (state.contador % 2 == 0) {
+      return {
+        ...state,
+        contador: state.contador + 1,
+      };
+    } else {
+      return {
+        ...state,
+        contador: state.contador + 2,
+      };
+    }
+  } else if (action.type === ASYNC) {
+      return setTimeout({
+        ...state,
+        contador: state.contador + 5
+      }, "5000")
+  } else {
+    return state;
+  }
 }
 
 module.exports = contador;
